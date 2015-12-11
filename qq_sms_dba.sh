f8d25ec402e4dadb83cee86a4963ce0ab1a847f5
huangzz6 <hzz1989@vip.qq.com>
2015-12-11 16:16:56 +0800

    renew

--- qq_sms_dba.sh
+++ qq_sms_dba.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #导入jq环境变量，否则执行时会找不到命令，根据具体配置填写
-export PATH=$PATH:/usr/local/bin/
+#export PATH=$PATH:/usr/local/bin/
 
 #发送的群号
 Gnumber=$1
@@ -9,7 +9,7 @@ Gnumber=$1
 # 你login.pl中定义的host和port
 API_ADDR="127.0.0.1:5011"
 
-# 处理下编码，用于合并告警内容的标题和内容，即$2和$3
+# 处理下编码，用于合并告警内容的标题和内容，即$2
 message=`echo -e "$2"|od -t x1 -A n -v -w10000 | tr " " %`
 
 # 获取gid，需要安装jd处理json
