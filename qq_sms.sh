258c7e3192707fbc425a29d78544a942faac697b
huangzhongzhang <hzz1989@vip.qq.com>
2017-03-16 11:42:57 +0800

    删除没用的注释

--- qq_sms.sh
+++ qq_sms.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# 导入jq环境变量，否则执行时会找不到命令，根据具体配置填写
-# export PATH=$PATH:/usr/local/bin/
-
 # 发送的群号
 Gnumber=$1
 
@@ -14,5 +11,6 @@ message=$(echo -e "$2" | od -t x1 -A n -v -w10000 | tr " " %)
 
 # 这没什么好说的
 api_url="http://$API_ADDR/openqq/send_group_message?uid=$Gnumber&content=$message"
-
+set -x
 curl $api_url
+set +x
